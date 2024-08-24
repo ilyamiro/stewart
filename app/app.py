e5d7431e8baf9b1211565a2575005df6d0f4212b
ilyamiro <ilyamiro.workemail@gmail.com>
2024-08-24 15:54:07 +0200

    Added more text-based utils, continued progressing on creating an App instance. Config changes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 # Standart library imports
 import logging
+import random
 import threading
 import time
 import os
@@ -7,6 +8,8 @@ import json
 
 # Third-party imports
 from playsound import playsound
+import g4f.Provider
+from g4f.client import Client as GPTClient
 
 # Local imports
 from audio.input import STT
@@ -14,7 +17,7 @@ from audio.output import ttsi
 
 from logs import get_logger
 
-from utils import yaml_load, json_load
+from utils import *
 
 from tree import Tree
 
@@ -25,6 +28,11 @@ logger = get_logger("app")
 
 
 class App:
+    """
+    Main running instance of an application behind the GUI (plan)
+    For further reference, VA = Voice Assistant
+    """
+
     @staticmethod
     def sound_effect_decorator(func):
         """Sound effect decorator for initializing"""
@@ -39,35 +47,62 @@ class App:
 
     @sound_effect_decorator
     def __init__(self):
-        self.lang = "ru"
-
+        # Configration file
         self.config = yaml_load(CONFIG_FILE)
+        self.lang = self.config["lang"]["prefix"]
 
+        logger.info("Configuration file loaded")
+
+        self.trigger_needed = None
+        if self.config["trigger"]["trigger-mode"] == "timed":
+            self.trigger_needed = True
+
+        # data tree initializing
         self.tree = Tree()
+        self.tree_init()
+
+        logger.info("Data tree initialized")
 
         # voice recognition
         self.stt = STT(self.lang)
 
+        logger.debug("Speech to text instance initialized")
+
         # history of requests
         self.history_file = f"{DIR}/history.json"
 
+        # gpt settings
+        self.gpt_history = []
+        self.gpt_client = GPTClient()
+        self.gpt_provider = getattr(g4f.Provider, self.config["gpt"]["provider"])
+        self.gpt_start = [{"role": "user", "content": self.config["gpt"][f"start-prompt-{self.lang}"]},
+                          {"role": "system", "content": self.config["gpt"][f"start-answer-{self.lang}"]}]
+
+        logger.info("Initialized GPT settings and a GPT client")
+
+        # starting the voice recognition
         self.recognition_thread = threading.Thread(target=self.recognition)
         self.recognition_thread.start()
 
         logger.debug(f"Recognition thread started with name: {self.recognition_thread.name}")
 
-    def command_tree_init(self):
+    def recognition(self):
+        while True:
+            for word in self.stt.listen():
+                result = self.remove_trigger_word(word)
+                if result != "blank":
+                    self.handle(result)
+
+    def trigger_counter(self, times):
+        pass
+
+    def trigger_change(self):
+        pass
+
+    def tree_init(self):
 
         def add_command(com: tuple, handler: str, parameters: dict = None, synthesize: list = None,
                         synonyms: dict = None, equivalents: list = None):
-            # if not synonyms:
-            #     synonyms = {}
-            # if not synthesize:
-            #     synthesize = []
-            # if not parameters:
-            #     parameters = {}
-            # if not equivalents:
-            #     equivalents = []
             self.tree.add_commands(
                 {com: {"handler": handler, "parameters": parameters, "synthesize": synthesize, "synonyms": synonyms,
                        "equivalents": equivalents}})
@@ -103,22 +138,73 @@ class App:
             json.dump(history, f, indent=4)
 
     def handle(self, request):
-        if not request:
-            ttsi.say(random.choice(self.config[f"answers-{self.lang}"]))
+        if not request and self.config["trigger"]["trigger-mode"] != "disabled":
+            # if the request does not contain anything and the trigger word is required,
+            # it means that the user called for VA with a trigger word
+            # (that got removed by self.remove_trigger_word)
+            # and did not specify the command; therefore, we answer with a default phrase
+            ttsi.say(random.choice(self.config[f"answers-{self.lang}"]["default"]))
+        else:
+            # checking whether the request contains multiple commands
+            total = self.multihandle(request)
+            if len(total) == 1:
+                # if there is only one command, process it
+                result = self.tree.find_command(total[0])
+                if result:
+                    result = list(result)
+                    result.extend([total[0], request])
+                    self.do_synth(result)
+            elif len(total) > 1:
+                pass
+            elif not total and self.config["gpt"]["state"]:
+                pass
+                # If something was said by user after the trigger word, but no commands were recognized,
+                # then this phrase is being sent to gpt model for answering
 
-    def multihandle(self):
+    def do(self, request):
+        if request[2]:
+            ttsi.say(random.choice(*request[2]))
+        thread = threading.Thread(target=getattr(self.find_arch(request[0]), request[0]),
+                                  kwargs={"parameters": request[1], "command": request[3],
+                                          "request": request[4]})
+        thread.start()
+
+    def find_arch(self, request):
         pass
 
-    def recognition(self):
-        while True:
-            for word in self.stt.listen():
-                result = self.remove_trigger_word(word)
-                if result != "blank":
-                    self.handle(result)
+    def multihandle(self, request):
+        list_of_commands, current_command = [], []
+        split_request = request.split()
+        for word in split_request:
+            if word in self.tree.first_words:
+                if current_command:
+                    list_of_commands.append(current_command)
+                if word in self.config["command-spec"][f"no-multi-first-words-{self.lang}"]:
+                    current_command = split_request[split_request.index(word):]
+                    list_of_commands.append(current_command)
+                    current_command = []
+                    break
+                current_command = [word]
+            else:
+                if current_command and word != self.config["command-spec"][f"connect-word-{self.lang}"]:
+                    current_command.append(word)
+                elif not current_command and word != self.config["command-spec"][f"connect-word-{self.lang}"]:
+                    pass
+        if current_command:
+            list_of_commands.append(current_command)
+        return list_of_commands
 
-    def remove_trigger_word(self, request):
-        for trigger in self.config["configuration"]["triggers"]:
-            if trigger in request:
-                request = " ".join(request.split(trigger)[1:])[1:]
-                return request
-        return "blank"
+    def answer_gpt(self, query):
+        answer = self.gpt_client.chat.completions.create(
+            messages=[*self.gpt_start, *self.gpt_history, {"role": "user", "content": query}],
+            provider=self.gpt_provider,
+            stream=False,
+            model=g4f.models.default
+        ).choices[0].message.content
+
+        self.gpt_history.extend([{"role": "user", "content": query}, {"role": "system", "content": answer}])
+        if len(self.gpt_history) >= 8:
+            self.gpt_history = self.gpt_history[2:]
+
+        answer = numbers_to_strings(answer)
+        return answer
--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,13 @@ import subprocess
 import logging
 import sys
 import yaml
+import re
+
+from num2words import num2words
+from words2numsrus import NumberExtractor
+import g4f
+
+extractor = NumberExtractor()
 
 
 def run(*args, stdout: bool = False):
@@ -29,3 +36,124 @@ def json_load(path: str):
     if os.path.exists(path):
         with open(path) as file:
             return json.load(file)
+
+
+def get_part_of_day(hour):
+    if 3 <= hour < 12:
+        return "доброе утро"
+    elif 12 <= hour < 16:
+        return "доброго дня"
+    elif 16 <= hour < 23:
+        return "доброго вечера"
+    else:
+        return "доброй ночи"
+
+
+def get_hour_suffix(hour):
+    if 11 < hour < 20:
+        return "ов"
+    else:
+        last_digit = hour % 10
+        if last_digit == 1:
+            return ""
+        elif 1 < last_digit < 5:
+            return "а"
+        else:
+            return "ов"
+
+
+def get_minute_suffix(minutes):
+    if 10 < minutes < 20:
+        return ""
+    else:
+        last_digit = minutes % 10
+        if last_digit == 1:
+            return "а"
+        elif 1 < last_digit < 5:
+            return "ы"
+        else:
+            return ""
+
+
+def get_second_suffix(seconds):
+    if 10 < seconds < 20:
+        return ""
+    else:
+        last_digit = seconds % 10
+        if last_digit == 1:
+            return "а"
+        elif 1 < last_digit < 5:
+            return "ы"
+        else:
+            return ""
+
+
+def get_currency_suffix(amount):
+    last_two_digits = amount % 100
+    last_digit = amount % 10
+
+    if 10 < last_two_digits < 20:
+        return "ей"
+    elif last_digit == 1:
+        return "ь"
+    elif 1 < last_digit < 5:
+        return "я"
+    else:
+        return "ей"
+
+
+def get_degree_suffix(degrees):
+    last_digit = degrees % 10
+
+    if 10 < degrees < 20:
+        return "ов"
+    elif last_digit == 1:
+        return ""
+    elif 1 < last_digit < 5:
+        return "а"
+    else:
+        return "ов"
+
+
+def numbers_to_strings(text: str):
+    # Using regular expression to find all numbers in the string
+    all_numbers = re.findall(r"[-+]?\d*\.\d+|\d+", text)
+
+    # Converting the numbers from string to their respective data types
+    all_numbers = [int(num) if num.isdigit() else float(num) for num in all_numbers]
+
+    for number in all_numbers:
+        word = num2words(int(number), lang="ru")
+        text = text.replace(str(number), word)
+
+    return text
+
+
+def replace_yo_with_e(input_string):
+    return input_string.replace("ё", "е")
+
+
+def kelvin_to_c(k):
+    return int(k - 273.15)
+
+
+def extract_number(input_string):
+    matches = re.findall(r'\d+', input_string)
+
+    if matches:
+        if len(matches) == 1:
+            return int(matches[0])
+        else:
+            return tuple(map(int, matches))
+    else:
+        return None
+
+
+def find_num_in_list(lst):
+    return extract_number(extractor.replace_groups(replace_yo_with_e(" ".join(lst))))
+
+
+def find_num_in_string(lst):
+    return extract_number(extractor.replace_groups(replace_yo_with_e(lst)))
+
+
